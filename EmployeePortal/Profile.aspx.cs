1599a8fb4dbbcb9108821a2d7543408032364349
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-03-07 11:30:51 +0530

    aspnet project day 1

---
+++ EmployeePortal/Profile.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeePortal
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["EmployeeId"]!=null)
+            {
+                if(!IsPostBack)
+                {
+                    getEmployeeDetails();
+                }
+                
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void getEmployeeDetails()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from EmpRegister where Srno=@empid", con);
+            cmd.Parameters.AddWithValue("@empid", Session["EmployeeId"].ToString());
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.HasRows)
+            {
+                sdr.Read();
+                txtName.Text = sdr.GetValue(1).ToString();
+                txtEmail.Text = sdr.GetValue(2).ToString();
+                txtContact.Text = sdr.GetValue(3).ToString();
+                txtdept.Text = sdr.GetValue(4).ToString();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("update EmpRegister set Name=@name,Email=@email,Contact=@contact where Srno=@empid",con);
+            cmd.Parameters.AddWithValue("@name",txtName.Text);
+            cmd.Parameters.AddWithValue("@email",txtEmail.Text);
+            cmd.Parameters.AddWithValue("@contact",txtContact.Text);
+            cmd.Parameters.AddWithValue("@empid", Session["EmployeeId"].ToString());
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            getEmployeeDetails();
+        }
+    }
+}
---
+++ EmployeePortal/Register.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeePortal
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("insert into EmpRegister values (@name,@email,@contact,@dept,@pwd)", con);
+            cmd.Parameters.AddWithValue("@name",txtName.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+            cmd.Parameters.AddWithValue("@dept", ddldept.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@pwd",txtPassword.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            txtName.Text = "";
+            txtContact.Text = "";
+            txtEmail.Text = "";
+            ddldept.SelectedIndex = 0;
+            txtPassword.Text = "";
+
+            this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Registered Successfully..!','','success');", true);
+
+        }
+    }
+}
