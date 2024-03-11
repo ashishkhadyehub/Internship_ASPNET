90bce69218e96a40593372c405921e355d7dc567
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-03-11 11:00:52 +0530

    asp net project day 2

---
+++ EmployeePortal/Admin.Master.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeePortal
+{
+    public partial class Admin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Admin"] != null)
+            {
+                lilogin.Visible = false;
+                lilist.Visible = true;
+                lilogout.Visible = true;
+            }
+            else
+            {
+                lilogin.Visible = true;
+                lilist.Visible = false;
+                lilogout.Visible = false;
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("AdminLogin.aspx");
+        }
+    }
+}
